ceda6000118db952b3548e2b15964860722c086c
Refaliya Vinandha Shalsabila <revaliya80@gmail.com>
2026-09-23 14:31:39 +0700

    Soal 1

--- JOBSHEET 7 - PW/buku/proses_tambah.php
+++ JOBSHEET 7 - PW/buku/proses_tambah.php
@@ -23,6 +23,10 @@ if (!is_numeric($tahun) || $tahun < 1900 || $tahun > 2026) {
 if (!is_numeric($stok) || $stok < 0) {
     $errors[] = "Stok tidak boleh negatif.";
 }
+// tambah validasi ISBN
+if ($isbn !== '' && !preg_match('/^[0-9-]+$/', $isbn)) {
+    $errors[] = "ISBN hanya boleh berisi angka dan tanda hubung (-).";
+}
 
 if (!empty($errors)) {
     $_SESSION['flash'] = ['type' => 'error', 'pesan' => implode(' ', $errors)];
