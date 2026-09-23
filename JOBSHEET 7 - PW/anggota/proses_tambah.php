578be8350d4998252e40f766ab5f26c86d828680
Refaliya Vinandha Shalsabila <revaliya80@gmail.com>
2026-09-23 14:52:22 +0700

    soal 2

--- JOBSHEET 7 - PW/anggota/proses_tambah.php
+++ JOBSHEET 7 - PW/anggota/proses_tambah.php
@@ -13,7 +13,9 @@ if ($nama === '') {
 if ($noAnggota === '') {
     $errors[] = "No. Anggota wajib diisi.";
 }
-
+if ($noHp !== '' && !preg_match('/^[0-9+\- ]+$/', $noHp)) {
+    $errors[] = "No. HP hanya boleh berisi angka, tanda plus (+), tanda hubung (-), dan spasi.";
+}
 if (!empty($errors)) {
     $_SESSION['flash'] = ['type' => 'error', 'pesan' => implode(' ', $errors)];
     header('Location: tambah.php');
